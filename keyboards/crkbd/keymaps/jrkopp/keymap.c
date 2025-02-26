0401b10746dc49362877e70be79918ae00612de4
John Kopp <jrkopp@optonline.net>
2025-02-26 13:57:30 -0500

    Added Cap Word and Achordion to prevent problems on home row with roll overs

--- keyboards/crkbd/keymaps/jrkopp/keymap.c
+++ keyboards/crkbd/keymaps/jrkopp/keymap.c
@@ -32,6 +32,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum_keycodes.h"
 #include <action_layer.h>
 
+#include "process_combo.h"
+#include "caps_word.h"
+
+#include "features/achordion.h"
+
 enum layer_names {
     BASE = 0,
     SYMBOL,
@@ -42,8 +47,8 @@ enum layer_names {
 
 enum custom_keycodes {
     KC_LT_LP = SAFE_RANGE,
-    KC_RT_RP,
-    KC_BG_AP,
+    KC_GT_RP,
+    KC_AM_EX,
     MNW,
     MSW,
     MSE,
@@ -69,6 +74,16 @@ enum custom_keycodes {
 #define CUT LCTL(KC_X)
 #define PASTE LCTL(KC_V)
 
+enum combo_events {
+    VC_CW_TOGG = 0,
+};
+
+const uint16_t PROGMEM jc_combo[] = {KC_V, KC_C, COMBO_END};
+
+combo_t key_combos[] = {
+  [JC_CW_TOGG] = COMBO_ACTION(jc_combo)
+};
+/* COMBO_ACTION(x) is same as COMBO(x, KC_NO) */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -76,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------|.           ,---------------------------------------------------------------.
        KC_GRV,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC,               KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|            |---------+--------+--------+--------+--------+--------+--------|
-      KC_MINS,      LGA,    LAS,     LCD,     LSF,    KC_G, KC_LT_LP,             KC_RT_RP,    KC_H,     RSJ,     RCK,     RAL,    RGSC, KC_QUOT,
+      KC_MINS,      LGA,    LAS,     LCD,     LSF,    KC_G, KC_LT_LP,             KC_GT_RP,    KC_H,     RSJ,     RCK,     RAL,    RGSC, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|            |---------+--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
+     KC_AM_EX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------+--------|            |---------+--------+--------+--------+--------+--------+--------|
          LT(MEDIA, KC_ESC),   LT(NAV,KC_TAB),   LT(SYMBOL,KC_SPACE),            LT(SYMBOL, KC_ENTER),  LT(MOUSE, KC_BSPC),  LT(MEDIA, KC_DEL)
          //`-------------------------------------------------------'            `-----------------------------------------------------------'
@@ -87,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------|.           ,---------------------------------------------------------------.
      KC_TILDE, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_LBRC,               KC_RBRC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------+--------|            |---------+--------+--------+--------+--------+--------+--------|
-       KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_LT_LP,             KC_RT_RP,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+       KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_LT_LP,             KC_GT_RP,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   //|--------+--------+--------+--------+--------+--------+--------|            |---------+--------+--------+--------+--------+--------+--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------+--------|            |---------+--------+--------+--------+--------+--------+--------|
@@ -107,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
     [MOUSE] = LAYOUT_split_3x6_3_ex2(
-  //,--------------------------------------------------------------.            ,--------------------------------------------------------------.
+  //,--------------------------------------------------------------.     yyf       ,--------------------------------------------------------------.
       MS_WHLU, MS_BTN1,     MNW,   MS_UP,     MNE, MS_ACL0, _______,              _______, _______,   PASTE,    COPY,     CUT,    UNDO, _______,
   //|--------+--------+--------+--------+--------+--------|--------|            |--------|--------+--------+--------+--------+--------+--------|
       MS_WHLD, MS_BTN2, MS_LEFT, _______, MS_RGHT, MS_ACL1, _______,              _______, _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
@@ -142,6 +157,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
     case KC_LT_LP:
       if (record->event.pressed) {
@@ -150,22 +166,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           tap_code16(KC_LPRN);
         } else {
           // Send "<" when Shift is not held
-          tap_code16(KC_COMM);
+          tap_code16(KC_LT);
         }
       }
       return false; // Skip the "send key event" part
-    case KC_RT_RP:
+    case KC_GT_RP:
       if (record->event.pressed) {
         if (get_mods() & MOD_MASK_SHIFT) {
           // Send ")" when Shift is held
           tap_code16(KC_RPRN);
         } else {
           // Send ">" when Shift is not held
-          tap_code16(KC_LT);
+          tap_code16(KC_GT);
         }
       }
       return false; // Skip the "send key event" part
-    case KC_BG_AP:
+    case KC_AM_EX:
       if (record->event.pressed) {
         if (get_mods() & MOD_MASK_SHIFT) {
           // Send "!" when Shift is held
@@ -209,17 +225,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             mousekey_send();
         }
         return false; // Indicates that the keycode was handled
-        case MNE:
-          if (record->event.pressed) {
-              mousekey_on(KC_MS_RIGHT);
-              mousekey_on(KC_MS_UP);
-              mousekey_send();
-          } else {
-              mousekey_off(KC_MS_RIGHT);
-              mousekey_off(KC_MS_UP);
-              mousekey_send();
-          }
-          return false; // Indicates that the keycode was handled
+    case MNE:
+      if (record->event.pressed) {
+          mousekey_on(KC_MS_RIGHT);
+          mousekey_on(KC_MS_UP);
+          mousekey_send();
+      } else {
+          mousekey_off(KC_MS_RIGHT);
+          mousekey_off(KC_MS_UP);
+          mousekey_send();
+      }
+      return false; // Indicates that the keycode was handled
     default:
       return true; // Process all other keycodes normally
   }
@@ -256,4 +272,42 @@ layer_state_t layer_state_set_user(layer_state_t state) {
       break;
     }
   return state;
+}
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  switch(combo_index) {
+
+    case VC_CW_TOGG:
+      if (pressed) {
+        if (!is_caps_word_on()) {
+          caps_word_on();
+          rgblight_sethsv(30, 255, 48); // orange at 25% brightness
+        } else {
+          caps_word_off();
+          rgblight_sethsv(0, 0, 48); // white at 25% brightness
+        }
+      }
+      break;
+  }
+}
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            rgblight_sethsv(0, 0, 48); // white at 25% brightness
+            return false;  // Deactivate Caps Word.
+    }
 }
--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -1,1 +1,6 @@
 DEFAULT_FOLDER = crkbd/rev1
+
+COMBO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+SRC += features/achordion.c
